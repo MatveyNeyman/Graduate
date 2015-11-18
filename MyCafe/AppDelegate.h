2866955fca34afd370a2847fb892ef29145c9dfc
User <user@users-mac.local>
2015-11-18 19:05:14 +0300

    Initial Commit

---
+++ MyCafe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCafe
+//
+//  Created by User on 11/18/15.
+//  Copyright © 2015 Harman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
