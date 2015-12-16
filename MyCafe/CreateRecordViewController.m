90a3617059b9d72284763e20194b02f8b9c9b902
User <user@users-mac.local>
2015-12-16 19:57:31 +0300

    Rating and Price are done

--- MyCafe/CreateRecordViewController.m
+++ MyCafe/CreateRecordViewController.m
@@ -9,7 +9,7 @@
 #import "CreateRecordViewController.h"
 #import "TypeSelectorViewController.h"
 #import <CoreLocation/CoreLocation.h>
-#import <AddressBookUI/AddressBookUI.h>
+#import <HCSStarRatingView/HCSStarRatingView.h>
 
 
 @interface CreateRecordViewController () <CLLocationManagerDelegate>
@@ -55,7 +55,6 @@
 }
 
 - (void)viewWillAppear:(BOOL)animated {
-    
     if (self.typeSelectorViewController.currentType) {
         self.selectedType = self.typeSelectorViewController.currentType;
     }
@@ -65,6 +64,7 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
 
 
@@ -107,15 +107,12 @@
 - (IBAction)useCurrentLocationSwitchTriggered:(id)sender {
     NSLog(@"Current Location Switch Triggered");
     UISwitch *switcher = (UISwitch *) sender;
-    switch (switcher.isOn) {
-        case YES:
-            NSLog(@"Switch is ON");
-            [self feedAddress];
-            break;
-        case NO:
-            NSLog(@"Switch is OFF");
-            self.addressTextField.text = nil;
-            break;
+    if (switcher.isOn) {
+        NSLog(@"Switch is ON");
+        [self feedAddress];
+    } else {
+        NSLog(@"Switch is OFF");
+        self.addressTextField.text = nil;
     }
 }
 
@@ -123,7 +120,7 @@
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     [self.locationManager requestWhenInUseAuthorization];
-
+    
     CLLocation *currentLocation = self.locationManager.location;
     
     // Initialize Geocoder object to get address from location coordinates (reverse geocoding)
@@ -132,28 +129,94 @@
     }
     
     [self.geocoder reverseGeocodeLocation:currentLocation completionHandler:
-        ^(NSArray<CLPlacemark *> *placemarks, NSError *error) {
-            if ([placemarks count] > 0) {
-                CLPlacemark *currentPlacemark = [placemarks objectAtIndex:0];
-                
-                // placemarks array contains addressDictionary dictionary with tge following array as one of the values
-                NSArray *currentAddressArray = [currentPlacemark.addressDictionary objectForKey:@"FormattedAddressLines"];
-                
-                // Navigate array and build final address
-                NSString *currentAddress = @"";
-                for (int i = 0; i < currentAddressArray.count; i++) {
-                    NSString *str = currentAddressArray[i];
-                    currentAddress = [currentAddress stringByAppendingString:str];
-                    if (i >= 0 && i < currentAddressArray.count - 1) {
-                        currentAddress = [currentAddress stringByAppendingString:@", "];
-                    }
-                }
-                // Feed address field
-                self.addressTextField.text = currentAddress;
-            }
-    }];
-    
-    
+     ^(NSArray<CLPlacemark *> *placemarks, NSError *error) {
+         if ([placemarks count] > 0) {
+             CLPlacemark *currentPlacemark = [placemarks objectAtIndex:0];
+             
+             // placemarks array contains addressDictionary dictionary with tge following array as one of the values
+             NSArray *currentAddressArray = [currentPlacemark.addressDictionary objectForKey:@"FormattedAddressLines"];
+             
+             // Navigate array and build final address
+             NSString *currentAddress = @"";
+             for (int i = 0; i < currentAddressArray.count; i++) {
+                 NSString *str = currentAddressArray[i];
+                 currentAddress = [currentAddress stringByAppendingString:str];
+                 if (i >= 0 && i < currentAddressArray.count - 1) {
+                     currentAddress = [currentAddress stringByAppendingString:@", "];
+                 }
+             }
+             // Feed address field
+             self.addressTextField.text = currentAddress;
+         }
+     }];
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ([cell.reuseIdentifier isEqual:@"Rating"]) {
+        NSLog(@"Rating cell found");
+        [self createStarRatingViewForCell:cell
+                      withEmptyImageNamed:@"emptyStar"
+                     withFilledImageNamed:@"filledStar"
+                                   action:@selector(didChangeRating:)];
+    }
+    if ([cell.reuseIdentifier isEqual:@"Price"]) {
+        NSLog(@"Price cell found");
+        [self createStarRatingViewForCell:cell
+                      withEmptyImageNamed:@"emptyCoin"
+                     withFilledImageNamed:@"filledCoin"
+                                   action:@selector(didChangePrice:)];
+    }
+}
+
+- (void)createStarRatingViewForCell:(UITableViewCell *)cell
+                     withEmptyImageNamed:(NSString *)emptyImage
+               withFilledImageNamed:(NSString *)filledImage
+                             action:(SEL)action {
+    HCSStarRatingView *starRatingView = [HCSStarRatingView new];
+    starRatingView.maximumValue = 5;
+    starRatingView.minimumValue = 0;
+    starRatingView.value = 0;
+    starRatingView.emptyStarImage = [UIImage imageNamed:emptyImage];
+    starRatingView.filledStarImage = [UIImage imageNamed:filledImage];
+    [starRatingView addTarget:self action:action forControlEvents:UIControlEventValueChanged];
+    [cell.contentView addSubview:starRatingView];
+    starRatingView.translatesAutoresizingMaskIntoConstraints = NO;
+    [[NSLayoutConstraint constraintWithItem:starRatingView
+                                  attribute:NSLayoutAttributeCenterY
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:cell.contentView
+                                  attribute:NSLayoutAttributeCenterY
+                                 multiplier:1
+                                   constant:0] setActive:YES];
+    [[NSLayoutConstraint constraintWithItem:cell.contentView
+                                  attribute:NSLayoutAttributeTrailingMargin
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:starRatingView
+                                  attribute:NSLayoutAttributeTrailing
+                                 multiplier:1
+                                   constant:10] setActive:YES];
+    [[NSLayoutConstraint constraintWithItem:starRatingView
+                                  attribute:NSLayoutAttributeHeight
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:nil
+                                  attribute:NSLayoutAttributeNotAnAttribute
+                                 multiplier:1
+                                   constant:28] setActive:YES];
+    [[NSLayoutConstraint constraintWithItem:starRatingView
+                                  attribute:NSLayoutAttributeWidth
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:nil
+                                  attribute:NSLayoutAttributeNotAnAttribute
+                                 multiplier:1
+                                   constant:180] setActive:YES];
+}
+
+- (IBAction)didChangeRating:(HCSStarRatingView *)sender {
+    NSLog(@"Changed rating to %.1f", sender.value);
+}
+
+- (IBAction)didChangePrice:(HCSStarRatingView *)sender {
+    NSLog(@"Changed price to %.1f", sender.value);
 }
 
 @end
